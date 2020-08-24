e8602154c5b51c6a271c0f5f1bcecc1f843eef73
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 01:12:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-your_hash ={"name" => "Blen", "weight" => "120", "hobby"}
+your_hash ={"name" => "Blen", "weight" => "120", "hobby"=> "sleep"}
 end
 
 
